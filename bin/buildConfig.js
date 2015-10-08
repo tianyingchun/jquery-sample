b0d77676494d94897b7293035014fcf8d11c0a49
tianyingchun <tianyingchun@Outlook.com>
2015-10-08 18:09:22 +0800

    updates, update config

---
+++ bin/buildConfig.js
@@ -0,0 +1,112 @@
+var path = require('path');
+var url = require('url');
+var debug = require('debug');
+var config = require('../build.config.js');
+var _ = require('lodash');
+
+var logger = debug('server:serverRenderParams');
+
+/**
+ * Dynamic load routes, resources for isomorphic server via ./build.config.js
+ * only can be used in node environment.
+ * @param  {Object} req express get req
+ * @param  {Object} env node process.env.NODE_ENV
+ */
+var getRenderParams = function (req, env) {
+  var routes;
+  var jsBundles = [];
+  var cssBundles = [];
+  // The sub project, the current load project.
+  var fundProject = null;
+
+  var projects = config.projects;
+  var options = config.options;
+
+  // iso static cdn root.
+  var cdnRoot = env === 'development' ? options.assets.dev : options.assets.prod;
+
+  // '/workspace/list?name=tyc&type=1&other'
+  // var url = req.url;
+
+  // { name: 'tyc', type: '1', other: '' }
+  var query = req.query;
+
+  // var params = req.params;
+  // '/workspace/list'
+  var urlPath = req.path;
+
+  // logger('url path', url, query, urlPath);
+
+  _.each(projects, function (project, projectName) {
+    var _project = null;
+    _.each(project, function (subProject, subProjectName) {
+      // logger('subporject: ', subProject)
+      if (subProject.match && subProject.match.test(urlPath)) {
+        _project = subProject;
+        // project name.
+        _project.projectName = projectName;
+        // sub project name.
+        _project.subProjectName = subProjectName;
+
+        // the project meta info.
+        _project.metaInfo = project._metaInfo || {};
+
+        return false;
+      }
+    });
+    if (_.isObject(_project)) {
+      // logger('found: ', _project);
+      fundProject = _project
+
+      return false;
+    }
+  });
+
+  if (_.isObject(fundProject)) {
+    var version = fundProject.metaInfo.version || '';
+
+    // generate all css bundle files.
+    _.each(fundProject.cssBundles || [], function (css) {
+      var linkHref = url.resolve(cdnRoot, _.template(css)({
+        version: version
+      }));
+      var name = path.basename(linkHref, '.css');
+      var link = {
+        href: linkHref,
+        name: name
+      }
+      cssBundles.push(link);
+    });
+
+    // generate all js bundle files.
+    _.each(fundProject.jsBundles || [], function (js) {
+      jsBundles.push(url.resolve(cdnRoot, _.template(js)({
+        version: version
+      })));
+    });
+
+    var routePath;
+    try {
+      routePath = path.join(process.cwd(), fundProject.routes);
+      // the routes of current sub project(project).
+      routes = require(routePath);
+    } catch (e) {
+      console.log('require routes components has errors: ', routePath, e);
+      routes = null;
+    }
+  } else {
+    console.warn('=========can not find any matched routes!!!=========');
+  }
+
+  var result = {
+    project: fundProject,
+    routes,
+    jsBundles,
+    cssBundles
+  };
+
+  // logger('result: ', result);
+  return result;
+}
+
+module.exports = getRenderParams;
--- bin/simple.js
+++ bin/simple.js
@@ -3,7 +3,7 @@ var express = require('express');
 var cors = require('cors');
 var favicon = require('serve-favicon');
 var compression = require('compression');
-
+var getRenderParams = require('./buildConfig');
 var app = express();
 var NODE_ENV = app.get('env') || 'production';
 var port = process.env.PORT || 40000;
@@ -14,6 +14,46 @@ app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
 // Use this middleware to serve up static files built into the dist directory
 app.use("/public", cors(), express.static(path.join(__dirname, '../public')));
 app.use("/shared", cors(), express.static(path.join(__dirname, '../shared')));
+app.use("/", function(req, res) {
+    // Resolve current server rendering params.
+  var configParams = getRenderParams(req, NODE_ENV);
+  var project = configParams.project;
+  var routes = configParams.routes;
+  var jsBundles = configParams.jsBundles;
+  var cssBundle = configParams.cssBundle;
+
+  if (!routes || !project)  {
+    console.log('router match failed in build.config.js, 404 not found!');
+    // should give 404.
+    res.status(404).send('Not found');
+    return;
+  }
+  let stylesHtml = cssBundles.map(function (cssLink) {
+    return ('<link name="'+cssLink.name+'" rel="stylesheet" type="text/css" href="'+cssLink.href+'">');
+  }).join('');
+
+  let scriptsHtml = jsBundles.map(function (jsLink) {
+    return ('<script src="' + jsLink + '"></script>');
+  }).join('');
+
+  var html = '<!DOCTYPE html>' +
+    ' <html>' +
+    '  <head>' +
+    '  <meta charset="utf-8">' +
+    '  <meta name="renderer" content="webkit">' +
+    '  <meta http-equiv="Cache-Control" content="no-siteapp">' +
+    '  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' +
+    '  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">' +
+    stylesHtml +
+    '</head>' +
+    '  <body>' +
+    '    <div id="react-view"></div>' +
+    scriptsHtml +
+    '  </body>' +
+    '</html>';
+
+  res.send(html);
+});
 
 var server = app.listen(port, function () {
   console.log('===Express server listening on port %d ===', server.address().port);
